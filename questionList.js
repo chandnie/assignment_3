d6c4956ba9c63c481479c66a9541fe33486447f0
chandnie <95160283+chandnie@users.noreply.github.com>
2022-01-01 18:53:18 +0530

    Create questionList.js

---
+++ questionList.js
@@ -0,0 +1,34 @@
+const questions = [
+  
+   {
+    question : "which fruits are for vitamin-c?",
+    answer : "Oranges",
+    options: ["oranges","lemons","apple cidger","watermelon"]
+  },
+  
+  {
+    question : "Which diet is best after exercise??",
+    answer : "Milk",
+    options: ["Only Water","Milk","Almonds","Nuts"]
+  },
+  
+  {
+    question : "How many hours do you exercise daily?",
+    answer : "60min",
+    options: ["60min","5min","0min","10min"]
+  },
+  
+   {
+    question : "What is the most essential in the exercise?",
+    answer : "water",
+    options: ["carbohydrate drinks"," milk","water","beer"]
+  },
+  {
+    question : "What is your last exercise?",
+    answer : "Cycling",
+    options: ["Jumping","Running","Skipping","Cycling"]
+  },
+   
+]
+
+module.exports = questions;
